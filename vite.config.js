e21dce017dc8f1913e9340f136dadc3c36882adb
Nico Travassos <me@nicotravassos.co.za>
2024-09-03 02:42:30 +0200

    feat: add Tailwind CSS with additional plugins [MAIN-T-8]
    - Added Tailwind CSS via npm for modern utility-first CSS framework
    - Updated tailwind.config.js to include the new plugins
    - Created a guest layout component to standardize guest pages
    - Applied the guest layout component to the welcome view for consistent styling

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,43 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import {resolve} from 'path';
+import {homedir} from 'os';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({command, mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    let host = new URL(env.APP_URL).host
+    let homeDir = homedir()
+    let serverConfig = {}
+
+    if (homeDir) {
+        serverConfig = {
+            https: {
+                key: fs.readFileSync(
+                    resolve(homeDir, `.config/valet/Certificates/${host}.key`),
+                ),
+                cert: fs.readFileSync(
+                    resolve(homeDir, `.config/valet/Certificates/${host}.crt`),
+                ),
+            },
+            hmr: {
+                host
+            },
+            host
+        }
+    }
+
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+        server: serverConfig
+    }
 });
