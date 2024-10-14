a7a7c581faceff9fa81197777683ca5df1cf7f42
Nico Travassos <me@nicotravassos.co.za>
2024-10-14 15:52:00 +0200

    feat: add authentication views and components
    - Added authentication-related Blade components: AppLayout, authentication card, button, checkbox, input, label, and validation errors
    - Created login view for user authentication
    - Added dashboard view as a placeholder with TODO note for future design
    - Set up base layout in layouts/app.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    {{--    TODO: Design for Dashboard is still in progress. This area will be updated once the design is ready.--}}
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html class="h-full" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+    @livewireScripts
+</head>
+<body class="font-sans antialiased h-full dark:bg-black dark:text-white/50">
+<main>
+    {{ $slot }}
+</main>
+</body>
+</html>
