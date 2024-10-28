c92f2e9f6f28daca209de51b43b42501715a8694
Nico Travassos <me@nicotravassos.co.za>
2024-10-28 07:28:04 +0200

    chore: simplify Vite configuration for standard development environments

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,14 @@
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import fs from 'fs';
-import {resolve} from 'path';
-import {homedir} from 'os';
 
-export default defineConfig(({command, mode}) => {
-    const env = loadEnv(mode, process.cwd(), '')
-
-    let host = new URL(env.APP_URL).host
-    let homeDir = homedir()
-    let serverConfig = {}
-
-    if (homeDir) {
-        serverConfig = {
-            https: {
-                key: fs.readFileSync(
-                    resolve(homeDir, `.config/valet/Certificates/${host}.key`),
-                ),
-                cert: fs.readFileSync(
-                    resolve(homeDir, `.config/valet/Certificates/${host}.crt`),
-                ),
-            },
-            hmr: {
-                host
-            },
-            host
-        }
-    }
-
-    return {
-        plugins: [
-            laravel({
-                input: [
-                    'resources/css/app.css',
-                    'resources/js/app.js',
-                ],
-                refresh: true,
-            }),
-        ],
-        server: serverConfig
-    }
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
